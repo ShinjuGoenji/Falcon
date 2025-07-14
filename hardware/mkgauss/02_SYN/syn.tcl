bfe2c7bdbd7ef0c4fa6a80a5e9da47d7de58218f
daniel_kuo <daniel970275@gmail.com>
2025-07-15 07:41:14 +0800

    Update: 02_SYN

--- hardware/mkgauss/02_SYN/syn.tcl
+++ hardware/mkgauss/02_SYN/syn.tcl
@@ -8,7 +8,7 @@
 # (A) Global Parameters
 #======================================================
 set DESIGN "MKGAUSS"
-set CYCLE 2
+set CYCLE 1.7
 set INPUT_DLY [expr 0.5*$CYCLE]
 set OUTPUT_DLY [expr 0.5*$CYCLE]
 
