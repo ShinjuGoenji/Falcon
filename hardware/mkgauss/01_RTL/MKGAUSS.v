c789be7153226c06c7a7c1c860f7f3a3fc0338d0
daniel_kuo <daniel970275@gmail.com>
2025-07-17 15:14:43 +0800

    Update: modify MKGAUSS IO description

--- hardware/mkgauss/01_RTL/MKGAUSS.v
+++ hardware/mkgauss/01_RTL/MKGAUSS.v
@@ -27,6 +27,14 @@ module MKGAUSS #(
 //---------------------------------------------------------------------
 //   Input & Output
 //---------------------------------------------------------------------
+/**
+ * @input   ena             Enable signal of this module.
+ * @input   rng_valid       Valid signal from RNG.
+ * @input   rng             Two 64-bit random numbers from random number generator.
+ * @output  rng_extract     Response signal to RNG.
+ * @output  val_valid       Valid signal of output val.
+ * @output  val             32-bit signed output value (registered).
+ */
 input                     clk;
 input                     rst_n;
 input                     ena;
