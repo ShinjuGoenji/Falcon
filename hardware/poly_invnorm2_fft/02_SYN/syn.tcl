89ef6d0f033d11a07adeb47614d4435d1550b85f
daniel_kuo <daniel970275@gmail.com>
2025-09-23 06:36:56 +0800

    Update: POLY_INVNORM2_FFT 02_SYN 03_GATE

--- hardware/poly_invnorm2_fft/02_SYN/syn.tcl
+++ hardware/poly_invnorm2_fft/02_SYN/syn.tcl
@@ -8,7 +8,7 @@
 # (A) Global Parameters
 #======================================================
 set DESIGN "POLY_INVNORM2_FFT"
-set CYCLE 2.5
+set CYCLE 4.0 
 set INPUT_DLY [expr 0.5*$CYCLE]
 set OUTPUT_DLY [expr 0.5*$CYCLE]
 
