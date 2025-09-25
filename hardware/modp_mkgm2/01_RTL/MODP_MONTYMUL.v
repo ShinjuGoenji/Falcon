a5237650a544b26aecec9284a7194fb37c08be80
daniel_kuo <daniel970275@gmail.com>
2025-09-25 14:57:10 +0800

    Add: MODP_MKGM2

---
+++ hardware/modp_mkgm2/01_RTL/MODP_MONTYMUL.v
@@ -0,0 +1,114 @@
+/*
+ * Montgomery multiplication modulo p. The 'p0i' value is -1/p mod 2^31.
+ * It is required that p is an odd integer.
+ */
+module MODP_MONTYMUL (
+    // Input signals
+    clk,
+    rst_n,
+    ena,
+    in_valid,
+    a,
+    b,
+    p,
+    p0i,
+    isMQ,
+    // Output signals
+    out_valid,
+    d
+);
+
+//---------------------------------------------------------------------
+//   Parameter & Integer
+//---------------------------------------------------------------------
+localparam P_WIDTH = 31;
+
+//---------------------------------------------------------------------
+//   Input & Output
+//---------------------------------------------------------------------
+input                clk;
+input                rst_n;
+input                ena;
+input                in_valid;
+input  [P_WIDTH-1:0] a;
+input  [P_WIDTH-1:0] b;
+input  [P_WIDTH-1:0] p;
+input  [P_WIDTH-1:0] p0i;
+input                isMQ;
+    
+output reg               out_valid;
+output reg [P_WIDTH-1:0] d;
+
+//---------------------------------------------------------------------
+//   Reg & Wire
+//---------------------------------------------------------------------
+reg  [P_WIDTH-1:0]   a_reg;
+reg                  ena_reg;
+reg                  isMQ_reg;
+wire [P_WIDTH*2-1:0] a_b;
+reg  [P_WIDTH*2-1:0] a_b_reg;
+wire [P_WIDTH*3-1:0] a_b_p0i;
+reg  [P_WIDTH*3-1:0] a_b_p0i_reg;
+wire [P_WIDTH+15:0]  w_q;
+wire [P_WIDTH*2-1:0] w_p;
+wire [P_WIDTH*2:0]   z_w_q;
+wire [P_WIDTH*2:0]   z_w_p;
+wire [P_WIDTH:0]     z_w_q_shift;
+wire [P_WIDTH:0]     z_w_p_shift;
+
+//---------------------------------------------------------------------
+//   Combinational Logic
+//---------------------------------------------------------------------
+assign a_b = a * b;
+assign a_b_p0i = a_b * p0i;
+assign w_q = a_b_p0i_reg[15:0] * p;
+assign w_p = a_b_p0i_reg[30:0] * p;
+
+assign z_w_q = a_b_reg + w_q;
+assign z_w_p = a_b_reg + w_p;
+assign z_w_q_shift = z_w_q[P_WIDTH*2:16];
+assign z_w_p_shift = z_w_p[P_WIDTH*2:31];
+
+always @(*) begin
+    if (ena_reg) begin
+        if (isMQ_reg) begin
+            if (z_w_q_shift >= p)
+                d = z_w_q_shift - p;
+            else
+                d = z_w_q_shift;
+        end
+        else begin
+            if (z_w_p_shift >= p)
+                d = z_w_p_shift - p;
+            else
+                d = z_w_p_shift;
+        end
+    end
+    else begin
+        d = a_reg;
+    end
+end
+
+//---------------------------------------------------------------------
+//   Sequential Logic
+//---------------------------------------------------------------------
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        a_reg <= 0;
+        ena_reg <= 0;
+        isMQ_reg <= 0;
+        out_valid <= 0;
+        a_b_reg <= 0;
+        a_b_p0i_reg <= 0;
+    end
+    else begin
+        a_reg <= a;
+        ena_reg <= ena;
+        isMQ_reg <= isMQ;
+        out_valid <= in_valid;
+        a_b_reg <= a_b;
+        a_b_p0i_reg <= a_b_p0i;
+    end
+end
+
+endmodule
