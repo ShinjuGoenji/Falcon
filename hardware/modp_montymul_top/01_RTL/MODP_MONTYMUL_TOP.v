485c17295231e6e18085b15539d0bdba2ad706d6
daniel_kuo <daniel970275@gmail.com>
2025-09-30 21:47:34 +0800

    Add: MODP_MONTYMUL_TOP

---
+++ hardware/modp_montymul_top/01_RTL/MODP_MONTYMUL_TOP.v
@@ -0,0 +1,181 @@
+`include "MODP_MONTYMUL.v"
+
+/*
+ * 
+ */
+module MODP_MONTYMUL_TOP #(
+    parameter BUS_WIDTH = 1,
+    parameter MUL_NUM = 1
+)(
+    // Input signals
+    clk,
+    rst_n,
+    in_valid_bus,
+    a_bus,
+    b_bus,
+    p_bus,
+    p0i_bus,
+    isMQ_bus,
+    // Output signals
+    out_valid_bus,
+    d_bus
+);
+
+//---------------------------------------------------------------------
+//   Parameter & Integer
+//---------------------------------------------------------------------
+localparam P_WIDTH = 31;
+
+integer i, j;
+
+//---------------------------------------------------------------------
+//   Input & Output
+//---------------------------------------------------------------------
+input                              clk;
+input                              rst_n;
+input      [BUS_WIDTH-1:0]         in_valid_bus;
+input      [P_WIDTH*BUS_WIDTH-1:0] a_bus;
+input      [P_WIDTH*BUS_WIDTH-1:0] b_bus;
+input      [P_WIDTH*BUS_WIDTH-1:0] p_bus;
+input      [P_WIDTH*BUS_WIDTH-1:0] p0i_bus;
+input      [BUS_WIDTH-1:0]         isMQ_bus;
+    
+output reg [BUS_WIDTH-1:0]         out_valid_bus;
+output reg [P_WIDTH*BUS_WIDTH-1:0] d_bus;
+
+//---------------------------------------------------------------------
+//   Reg & Wire
+//---------------------------------------------------------------------
+wire               in_valid_bus_w  [0:BUS_WIDTH-1];
+wire [P_WIDTH-1:0] a_bus_w         [0:BUS_WIDTH-1];
+wire [P_WIDTH-1:0] b_bus_w         [0:BUS_WIDTH-1];
+wire [P_WIDTH-1:0] p_bus_w         [0:BUS_WIDTH-1];
+wire [P_WIDTH-1:0] p0i_bus_w       [0:BUS_WIDTH-1];
+wire               isMQ_bus_w      [0:BUS_WIDTH-1];
+
+reg               i_valid [0:MUL_NUM-1];
+reg [P_WIDTH-1:0] a       [0:MUL_NUM-1];
+reg [P_WIDTH-1:0] b       [0:MUL_NUM-1];
+reg [P_WIDTH-1:0] p       [0:MUL_NUM-1];
+reg [P_WIDTH-1:0] p0i     [0:MUL_NUM-1];
+reg               isMQ    [0:MUL_NUM-1];
+reg               o_valid [0:MUL_NUM-1];
+reg [P_WIDTH-1:0] d       [0:MUL_NUM-1];
+
+reg grant [0:BUS_WIDTH-1];
+reg [$clog2(BUS_WIDTH)-1:0] kernel_to_bus_map [0:MUL_NUM-1];
+
+reg               out_valid_bus_comb [0:BUS_WIDTH-1];
+reg [P_WIDTH-1:0] d_bus_comb [0:BUS_WIDTH-1];
+
+//---------------------------------------------------------------------
+//   Submodule
+//---------------------------------------------------------------------
+genvar modp_montymul_idx;
+generate
+    for (modp_montymul_idx = 0; modp_montymul_idx < MUL_NUM; modp_montymul_idx = modp_montymul_idx + 1) begin
+        MODP_MONTYMUL u_MODP_MONTYMUL (
+            // Input signals
+            .clk(clk),
+            .rst_n(rst_n),
+            .in_valid(i_valid[modp_montymul_idx]),
+            .a(a[modp_montymul_idx]),
+            .b(b[modp_montymul_idx]),
+            .p(p[modp_montymul_idx]),
+            .p0i(p0i[modp_montymul_idx]),
+            .isMQ(isMQ[modp_montymul_idx]),
+            .i_bus(kernel_to_bus_map[modp_montymul_idx]),
+            // Output signals
+            .out_valid(o_valid[modp_montymul_idx]),
+            .d(d[modp_montymul_idx]),
+            .o_bus(tmp[modp_montymul_idx])
+            );
+    end
+endgenerate
+
+//---------------------------------------------------------------------
+//   Combinational Logic
+//---------------------------------------------------------------------
+/*
+ * Unpack input bus
+ */
+genvar i_bus_idx;
+generate
+    for (i_bus_idx = 0; i_bus_idx < BUS_WIDTH; i_bus_idx = i_bus_idx + 1) begin
+        assign in_valid_bus_w[i_bus_idx] = in_valid_bus[i_bus_idx];
+        assign a_bus_w[i_bus_idx]        = a_bus[P_WIDTH*(i_bus_idx+1)-1 -: P_WIDTH];
+        assign b_bus_w[i_bus_idx]        = b_bus[P_WIDTH*(i_bus_idx+1)-1 -: P_WIDTH];
+        assign p_bus_w[i_bus_idx]        = p_bus[P_WIDTH*(i_bus_idx+1)-1 -: P_WIDTH];
+        assign p0i_bus_w[i_bus_idx]      = p0i_bus[P_WIDTH*(i_bus_idx+1)-1 -: P_WIDTH];
+        assign isMQ_bus_w[i_bus_idx]     = isMQ_bus[i_bus_idx];
+    end
+endgenerate
+
+/*
+ * Arbiter
+ */
+always @(*) begin
+    for (j = 0; j < MUL_NUM; j = j + 1) begin
+        i_valid[j] = 1'b0;
+        a[j] = {P_WIDTH{1'b0}};
+        b[j] = {P_WIDTH{1'b0}};
+        p[j] = {P_WIDTH{1'b0}};
+        p0i[j] = {P_WIDTH{1'b0}};
+        isMQ[j] = 1'b0;
+        kernel_to_bus_map[j] = 0;
+    end
+    for (i = 0; i < BUS_WIDTH; i = i + 1) begin
+        grant[i] = 1'b0;
+    end
+    for (j = 0; j < MUL_NUM; j = j + 1) begin
+        for (i = 0; i < BUS_WIDTH; i = i + 1) begin
+            if (in_valid_bus_w[i] && ~grant[i]) begin
+                i_valid[j] = in_valid_bus_w[i];
+                a[j]       = a_bus_w[i];
+                b[j]       = b_bus_w[i];
+                p[j]       = p_bus_w[i];
+                p0i[j]     = p0i_bus_w[i];
+                isMQ[j]    = isMQ_bus_w[i];
+                grant[i]   = 1'b1;
+                kernel_to_bus_map[j] = i;
+                break;
+            end
+        end
+    end
+end
+
+/*
+ * Map kernel to bus
+ */
+always @(*) begin
+    for (i = 0; i < BUS_WIDTH; i = i + 1) begin
+        out_valid_bus_comb[i] = 0;
+        d_bus_comb[i] = 0;
+    end
+    for (j = 0; j < MUL_NUM; j = j + 1) begin
+        out_valid_bus_comb[kernel_to_bus_map[j]] = o_valid[j];
+        d_bus_comb[kernel_to_bus_map[j]] = d[j];
+    end
+end
+
+//---------------------------------------------------------------------
+//   Sequential Logic
+//---------------------------------------------------------------------
+genvar o_bus_idx;
+generate
+    for (o_bus_idx = 0; o_bus_idx < BUS_WIDTH; o_bus_idx = o_bus_idx + 1) begin
+        always @(posedge clk or negedge rst_n) begin
+            if (!rst_n) begin
+                out_valid_bus[o_bus_idx] <= 0;
+                d_bus[P_WIDTH*(o_bus_idx+1)-1 -: P_WIDTH] <= 0;
+            end
+            else begin
+                out_valid_bus[o_bus_idx] <= out_valid_bus_comb[o_bus_idx];
+                d_bus[P_WIDTH*(o_bus_idx+1)-1 -: P_WIDTH] <= d_bus_comb[o_bus_idx];
+            end
+        end
+    end
+endgenerate
+
+
+endmodule
---
+++ hardware/modp_montymul_top/README.md
@@ -0,0 +1,42 @@
+# MODP_MONTYMUL_TOP
+
+```verilog
+module MODP_MONTYMUL_TOP #(
+    parameter BUS_WIDTH = 1,
+    parameter MUL_NUM = 1
+)(
+    // Input signals
+    clk,
+    rst_n,
+    in_valid_bus,
+    a_bus,
+    b_bus,
+    p_bus,
+    p0i_bus,
+    isMQ_bus,
+    // Output signals
+    out_valid_bus,
+    d_bus
+);
+```
+
+## Description
+
+Top module of MODP_MONTYMUL, which number of instances is parameterized by `MUL_NUM`. The number of master modules is also parameterized by `BUS_WIDTH`.
+
+## Latency
+
+| `MUL_NUM` |     |
+| :-------: | :-: |
+| **CYCLE** |     |
+
+## Performance
+
+|            | 40nm  |
+| :--------: | :---: |
+| `MUL_NUM`  |   2   |
+| **Period** | 0.0ns |
+| **#GATE**  |   0   |
+|  **AREA**  |  0.0  |
+
+## Future Optimization
